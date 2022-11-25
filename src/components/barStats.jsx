01e6a7be4ae517957e810281ecf8f9d2e58d721a
Roch Armengaud <armengaud.roch@live.fr>
2022-11-25 17:48:26 +0100

    Implementing some css

--- src/components/barStats.jsx
+++ src/components/barStats.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
 import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Legend, Bar } from 'recharts';
 
+import '../styles/barStats.css';
+
 export const BarStats = (props) => {
   // Ici les props seront les clés de la classe ({activity, stats})
   return (
-    <ResponsiveContainer width={835} height={320}>
-      <BarChart
-        width={60}
-        height={300}
-        data={props.sessions}
-        margin={{
-          top: 0,
-          right: 0,
-          left: 0,
-          bottom: 0,
-        }}
-        barGap={6}
-      >
-        <CartesianGrid strokeDasharray="3" vertical={false} />
-        <XAxis dataKey="day" domain={['dataMin + 1', 'dataMax + 1']} tickLine={false} dy={10} tickCount="7" />
-        <YAxis orientation="right" interval={'preserveStartEnd'} axisLine={false} allowDecimals={false} dataKey={'calories'} />
-        <Legend
-          height={40}
-          verticalAlign="top"
-          align="right"
-          iconType={'circle'}
-          wrapperStyle={{ top: 30, right: 100 }}
-          formatter={(value, entry, index) => <span className="text-color-class">{value}</span>}
-        />
-        <Bar dataKey="kilogram" fill="#282D30" borderRadius="20" barSize={10} radius={[10, 10, 0, 0]} name="Poids (kg)" />
-        <Bar dataKey="calories" fill="#E60000" barSize={10} radius={[10, 10, 0, 0]} name="Calories brûlées (kCal)" />
-      </BarChart>
-    </ResponsiveContainer>
+    <div className="barstats-container">
+      <ResponsiveContainer width={835} height={320}>
+        <BarChart
+          width={60}
+          height={300}
+          data={props.sessions}
+          margin={{
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
+          barGap={6}
+        >
+          <CartesianGrid strokeDasharray="3" vertical={false} />
+          <XAxis dataKey="day" domain={['dataMin + 1', 'dataMax + 1']} tickLine={false} dy={10} tickCount="7" />
+          <YAxis orientation="right" interval={'preserveStartEnd'} axisLine={false} allowDecimals={false} dataKey="calories" />
+          <Legend
+            height={70}
+            verticalAlign="top"
+            align="right"
+            iconType={'circle'}
+            wrapperStyle={{ top: 0, right: 20 }}
+            formatter={(value, entry, index) => <span className="text-color-class">{value}</span>}
+          />
+          <Bar dataKey="kilogram" fill="#282D30" borderRadius="20" barSize={10} radius={[10, 10, 0, 0]} name="Poids (kg)" />
+          <Bar dataKey="calories" fill="#E60000" barSize={10} radius={[10, 10, 0, 0]} name="Calories brûlées (kCal)" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
